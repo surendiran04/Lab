190849a00f980a068e839f300328c6e4f02b14d4
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-10-06 21:13:02 +0530

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                                                                                                                                                            
+read -p "Enter the file name to upload : " fname                                                                                                                                                       
+base64 $fname > $fname.b64                                                                                                                                                                             
+{"message": "uploaded $fname", "content": "--token"}                                                                                                                  
+CONTENT=$(cat $fname.b64)                                                                                                                                                                              
+echo "{\"message\": \"uploaded $fname\", \"content\": \"$CONTENT\"}" > payload.json                                                                                                                    
+                                                                                                                                                                                                       
+curl -X PUT -H "Authorization: token --token " \-H "Content-Type: application/json" \--data @payload.json \https://api.github.com/repos/Irfan-Fareeth/demolab/contents/
+$fname
